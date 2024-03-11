af83a730a67ed0f60f38aa3c3fdffb2554c9320c
kelechi-aims <kelechi@aims.edu.gh>
2024-03-11 14:09:05 +0100

    First API

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import routes from './routes/index.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(PORT);
